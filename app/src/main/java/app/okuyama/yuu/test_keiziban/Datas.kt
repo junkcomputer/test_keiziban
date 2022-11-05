3a65c3b857550b797b3a07cde1467658ba85ef84
junkcomputer <yutacreatorpc@gmail.com>
2022-11-05 20:07:43 +0900

    2022:11:05:20:07

---
+++ app/src/main/java/app/okuyama/yuu/test_keiziban/Datas.kt
@@ -0,0 +1,6 @@
+package app.okuyama.yuu.test_keiziban
+
+data class Datas (
+    val name: String = "",
+    val text: String = ""
+)
--- app/src/main/java/app/okuyama/yuu/test_keiziban/MainActivity.kt
+++ app/src/main/java/app/okuyama/yuu/test_keiziban/MainActivity.kt
@@ -1,11 +1,38 @@
 package app.okuyama.yuu.test_keiziban
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import app.okuyama.yuu.test_keiziban.databinding.ActivityMainBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+        val db = Firebase.firestore
+
+        binding.sousin.setOnClickListener {
+
+            val data = Datas(
+                name = binding.onamae.text.toString(),
+                text = binding.kakiko.text.toString()
+            )
+
+            db.collection("Test")
+                .add(data)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(TAG, "DocumentSnapshot written with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "Error adding document", e)
+                }
+        }
+
     }
 }
