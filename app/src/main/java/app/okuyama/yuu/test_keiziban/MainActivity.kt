b629b9ab68b5a39ffaece64be788d13e41b6190c
junkcomputer <yutacreatorpc@gmail.com>
2022-11-12 19:22:33 +0900

    2022.11.12(19:22)

--- app/src/main/java/app/okuyama/yuu/test_keiziban/MainActivity.kt
+++ app/src/main/java/app/okuyama/yuu/test_keiziban/MainActivity.kt
@@ -1,9 +1,16 @@
 package app.okuyama.yuu.test_keiziban
 
 import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.UserDictionary.Words
 import android.util.Log
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import app.okuyama.yuu.test_keiziban.databinding.ActivityMainBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -17,6 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         val db = Firebase.firestore
 
+//送信ボタン
         binding.sousin.setOnClickListener {
 
             val data = Datas(
@@ -24,6 +32,7 @@ class MainActivity : AppCompatActivity() {
                 text = binding.kakiko.text.toString()
             )
 
+            //書き込み
             db.collection("Test")
                 .add(data)
                 .addOnSuccessListener { documentReference ->
@@ -32,7 +41,54 @@ class MainActivity : AppCompatActivity() {
                 .addOnFailureListener { e ->
                     Log.w(TAG, "Error adding document", e)
                 }
+
+            val MainIntent: Intent = Intent(this,MainActivity::class.java)
+            startActivity(MainIntent)
         }
 
+        //読み込み
+        db.collection("Test")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    val layout = LinearLayout(this)
+                    val nameTextView = TextView(this)
+                    val textTextView = TextView(this)
+                    val idTextView = TextView(this)
+
+                    idTextView.text = document.id
+                    nameTextView.text = document.data.get("name").toString()
+                    textTextView.text = document.data.get("text").toString()
+
+                    nameTextView.textSize = 10.0f
+                    textTextView.textSize = 15.0f
+                    idTextView.textSize = 5.0f
+
+                    layout.addView(idTextView)
+                    layout.addView(nameTextView)
+                    layout.addView(textTextView)
+
+                    val lp = LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT
+                    )
+
+                    lp.gravity = Gravity.CENTER_VERTICAL
+                    nameTextView.layoutParams = lp
+                    textTextView.layoutParams = lp
+                    idTextView.layoutParams = lp
+
+                    binding.yomikomikun.addView(layout)
+                }
+            }
+
+            .addOnFailureListener { e ->
+                Log.w(TAG,"Error reading document",e)
+            }
+
+        binding.kousinkun.setOnClickListener {
+            val MainIntent: Intent = Intent(this,MainActivity::class.java)
+            startActivity(MainIntent)
+        }
     }
 }
