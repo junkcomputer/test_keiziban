77364fd3b96235560452a2f924e8070c59bb6710
junkcomputer <yutacreatorpc@gmail.com>
2023-02-11 19:24:19 +0900

    tamamokurosu

--- app/src/main/java/app/okuyama/yuu/test_keiziban/MainActivity.kt
+++ app/src/main/java/app/okuyama/yuu/test_keiziban/MainActivity.kt
@@ -11,6 +11,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.view.isVisible
 import app.okuyama.yuu.test_keiziban.databinding.ActivityAddBinding
 import app.okuyama.yuu.test_keiziban.databinding.ActivityMainBinding
 import com.google.firebase.firestore.ktx.firestore
@@ -27,12 +28,36 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
 
         val db = Firebase.firestore
+        val taro = MainAdapter()
 
-        val adapter = MainAdapter()
-
+        binding.recyclerView2.adapter = taro
         val rThreadName = intent.getStringExtra("threadName").toString()
         binding.documenttextview.text = rThreadName
 
+        val main = mutableListOf<Datas>()
+
+        db.collection("Thread")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result){
+                    //Log.d("xxxxxx" ,rThreadName.toString())
+                    if (document.id == rThreadName) {
+                        main.add(Datas(document.data.get("name").toString(),document.data.get("text").toString()))
+                    }
+                }
+                Log.d("neko", main.toString())
+                taro.updateThreads(main)
+            }
+
+        binding.kousinkun.setOnClickListener {
+            val MainIntent: Intent = Intent(this,MainActivity::class.java)
+            MainIntent.putExtra("threadName",rThreadName)
+            startActivity(MainIntent)
+        }
+    }
+}
+
+/*
         //送信ボタン
         binding.sousin.setOnClickListener {
 
@@ -41,8 +66,8 @@ class MainActivity : AppCompatActivity() {
                 text = binding.kakiko.text.toString()
             )
 
-
             //書き込み
+
             db.collection("Thread").document()
                 .set(data)
                 .addOnSuccessListener {
@@ -52,36 +77,24 @@ class MainActivity : AppCompatActivity() {
 
             val MainIntent: Intent = Intent(this,MainActivity::class.java)
             startActivity(MainIntent)
-        }
+        }*/
 
-        //読み込み
-        /*db.collection("Thread").document(rThreadName)
-            .get()
-            .addOnCompleteListener { result ->
-                val datas  = mutableListOf<Datas>()
-                    if (result.isSuccessful) {
-                        val document = result.result
-                            while (document != null) {
-                                datas.add(Datas(document.data?.get("name").toString(), document.data?.get("text").toString()))
-                            }
+
+
+//読み込み
+/*db.collection("Thread").document(rThreadName)
+    .get()
+    .addOnCompleteListener { result ->
+        val datas  = mutableListOf<Datas>()
+            if (result.isSuccessful) {
+                val document = result.result
+                    while (document != null) {
+                        datas.add(Datas(document.data?.get("name").toString(), document.data?.get("text").toString()))
                     }
-                adapter.updateThreads(datas)
             }
-            .addOnFailureListener { e ->
-                Log.w(TAG,"Error reading document",e)
-            }
-         */
-        db.collection("Thread")
-            .get()
-            .addOnSuccessListener { result ->
-                for (document in result){
-                    Log.d(TAG,"${document.id} => ${document.data.get("gotohana")}")
-                }
-            }
-
-        binding.kousinkun.setOnClickListener {
-            val MainIntent: Intent = Intent(this,MainActivity::class.java)
-            startActivity(MainIntent)
-        }
+        adapter.updateThreads(datas)
     }
-}
+    .addOnFailureListener { e ->
+        Log.w(TAG,"Error reading document",e)
+    }
+ */
